e8830136683c10f30a91474faa03e12c577caa1e
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-11-03 19:36:03 +0700

    chore(style): setup fonts

--- windi.config.ts
+++ windi.config.ts
@@ -5,4 +5,9 @@ export default defineConfig({
     include: ['**/*.{jsx,tsx,css}'],
     exclude: ['node_modules', '.git', '.next'],
   },
-})
+  theme: {
+    fontFamily: {
+      sans: ['Public Sans', 'system-ui', 'Segoe UI', 'Arial', 'sans-serif'],
+    },
+  },
+});
