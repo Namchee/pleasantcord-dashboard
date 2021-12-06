1beb6177b6c4c6f89beddc965412780aaf9407eb
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-12-06 21:42:47 +0700

    style: change illustrations

---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+function NotFoundPage(): JSX.Element {
+  return <></>;
+}
+
+export default NotFoundPage;
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -12,8 +12,8 @@ function Dashboard(): JSX.Element {
         <div className="grid place-items-center text-center">
           <Image
             src="/images/landing-dashboard.svg"
-            width={280}
-            height={280}
+            width={240}
+            height={240}
             title="Select server to manage"
             alt="Select server to manage"
           />
