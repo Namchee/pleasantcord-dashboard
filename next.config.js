37fa8848ee45afb0a809208b8a8bfe5f44775a93
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-12-08 20:10:02 +0700

    fix(deploy): add netlify next plugin

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const WindiCSSWebpackPlugin = require('windicss-webpack-plugin');
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  distDir: 'out',
   webpack(config) {
     config.plugins.push(new WindiCSSWebpackPlugin());
     return config;
