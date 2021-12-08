c00faa63fe23fbed1db298abab46274602261b43
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-12-08 20:24:44 +0700

    fix: revert back to old .next folder

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const WindiCSSWebpackPlugin = require('windicss-webpack-plugin');
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  distDir: 'out',
   webpack(config) {
     config.plugins.push(new WindiCSSWebpackPlugin());
     return config;
