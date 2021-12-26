eead54f992e53836a852dcaf3a91c390a569e4ed
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-12-26 15:02:21 +0700

    feat: add refresh token rotation

--- src/constant/provider.ts
+++ src/constant/provider.ts
@@ -1,1 +1,2 @@
 export const DISCORD = 'discord';
+export const REFRESH_GRANT = 'refresh_token';
--- src/next-auth.d.ts
+++ src/next-auth.d.ts
@@ -1,10 +1,21 @@
 /* eslint-disable camelcase */
+import { User } from 'next-auth';
+
 import 'next-auth/jwt';
 
 declare module 'next-auth/jwt' {
   interface JWT {
     userId?: string;
     accessToken?: string;
+    accessTokenExpires?: number;
     refreshToken?: string;
+  }
+}
+
+declare module 'next-auth' {
+  interface Session {
+    user?: User;
+    accessToken?: string;
+    error?: string;
   }
 }
